3ebaf835c41e41d29755135e6dd3438774b9e6dc
Roee Landesman <roee.landesman@gmail.com>
2019-03-04 15:37:56 -0800

    all

--- parseline.c
+++ parseline.c
@@ -1,19 +1,14 @@
+/*
+ERROR CHECKS: Double Bar, 512 Byte input 
+*/
+
 #include "parseline.h"
 
 int main(int argc, char **argv){
-	char *command;
-	int x = 0;
-	int byteLength;
-	int commandArgs;
 	char str[MAX_CMND_ARGS];
 
 	printf("line: ");
-	/* Check limitations, see README */
-	if(argc < 2){
-		printf("Error: command line arguments not supplied\n");
-		exit(1);
-	}
-
 	fgets(str, MAX_CMND_ARGS, stdin);
+	
 
 }
