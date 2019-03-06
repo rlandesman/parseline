d7768f216798b5dc6183498b9b3d2b019d32eed9
Roee Landesman <roee.landesman@gmail.com>
2019-03-05 18:54:31 -0800

    works

--- parseline.c
+++ parseline.c
@@ -1,7 +1,3 @@
-/*
-ERROR CHECKS: Double Bar, 512 Byte input 
-*/
-
 #include "parseline.h"
 
 int main(int argc, char **argv){
@@ -12,59 +8,57 @@ int main(int argc, char **argv){
 	char *temp;
 	int extra;
 	char ch;
+	int i;
+	char str2[MAX_CMND_ARGS];
 
 	printf("line: ");
 	fgets(str, MAX_CMND_ARGS, stdin);
 
-	/* Check to see if the command line is too long after fgets */
-	if (str[strlen(str)-1] != '\n') {
-        extra = 0;
-        while (((ch = getchar()) != '\n') && (ch != EOF))
-            extra = 1;
-        if(extra == 1){
-        	printf("Command too long\n");
-        	exit(1);
-        }
-    }
+	strncpy(str2,str, MAX_CMND_ARGS);
+	input fullString;
+	initInput(str2, &fullString);
+	inputErrorCheck(&fullString);
 
-	if(checkDoubleBars(str) == -1){
-		printf("Invalid null command\n");
-		exit(1);
-	}
-	checkAmibiousInput(str);
 
 	char *end_str;
 	char *token = strtok_r(str, "|", &end_str);
-
+	stage stageArray[MAX_PIPE_LEN];
 	while(token != NULL){
-		if(stageCount > MAX_PIPE_LEN){
-			printf("pipeline too deep\n");
-			exit(1);
-		}
 		char *end_token;
 		remove_newline_ch(token);
 
 		/* ERROR CHECK */
 		temp = getCommand(token);
-
-		if(checkDoubleAliIN(token) == -1){
-			printf("%s: Bad Input redirection\n", temp);
-			exit(1);	
-		}
-
-		if(checkDoubleAliOUT(token) == -1){
-			printf("%s: Bad output redirection\n", temp);
-			exit(1);	
-		}
 		printLine(stageCount, token);
 
 		char *token2 = strtok_r(token, " ", &end_token);
+
+		/*instantiating array of structs */
+		stageArray[stageCount].argc = 0;
+		stageArray[stageCount].stageNum = stageCount;
+
 		while(token2 != NULL){
+			stageArray[stageCount].argc++;
+			if(token2[0] == '<'){
+				token2 = strtok_r(NULL, " ", &end_token);
+				strcpy(stageArray[stageCount].input, token2);
+			}
+			if(token2[0] == '>'){
+				token2 = strtok_r(NULL, " ", &end_token);
+				strcpy(stageArray[stageCount].output,token2);
+			}
+			strcpy(stageArray[stageCount].argv[argCount],token);
 			token2 = strtok_r(NULL, " ", &end_token);
 			argCount++;
 		}
 		stageCount++;
 		token = strtok_r(NULL, "|", &end_str);
+	}
+
+	for(i = 0; i < stageCount; i++){
+		printf("OUTPUT: %s\n", stageArray[i].output);
+		printf("INPUT: %s\n", stageArray[i].input);
+
 	}
 }
 
@@ -91,48 +85,6 @@ void remove_newline_ch(char *line)
         line[new_line] = '\0';
 }
 
-int checkDoubleBars(char *str){
-	int i = 0;
-	for(; i < strlen(str); i++){
-		if(str[i] == '|'){
-			if(str[i-2] == '|'){
-				return -1;
-			}
-		}
-	}
-	return 0;
-}
-
-int checkDoubleAliIN(char *str){
-	int cnt = 0;
-	int i = 0;
-	for(; i < strlen(str); i++){
-		if(str[i] == '<'){
-			cnt++;
-		}
-	}
-	if(cnt > 1){
-		return -1;
-	} else {
-		return 0;
-	}
-}
-
-int checkDoubleAliOUT(char *str){
-	int cnt = 0;
-	int i = 0;
-	for(; i < strlen(str); i++){
-		if(str[i] == '>'){
-			cnt++;
-		}
-	}
-	if(cnt > 1){
-		return -1;
-	} else {
-		return 0;
-	}
-}
-
 char *getCommand(char *str){
 	char *temp = malloc(sizeof(strlen(str)));
 	int i;
@@ -148,26 +100,91 @@ char *getCommand(char *str){
 	return temp;
 }
 
-void checkAmibiousInput(char *str){
+int inputErrorCheck(input *in) {
+	int pipelen = 0;
+	int cmdargs = 0;
+	int inset = 0; 
+	int outset = 0; 
+	char *curCmd;
 	int i;
-	int flag = 0;
-	char *temp = strdup(str);
-	char *word;
-	word = strrchr(temp, '|');
-	char *tok = strtok(word, " ");
-	tok = strtok(NULL," ");
 
-	for(i = 0; i < strlen(str); i++){
-		if(str[i] == '<'){
-			flag = 1;
+	for (i = 0; i < in->size; i++) {
+		if (cmdargs == 0) { /*new command*/
+			if (in->words[i][0] == '|' || in->words[i][0] == '<' || in->words[i][0] == '>') {
+				fprintf(stderr, "Invalid null command.\n"); 
+				return 1;
+			} else {
+				curCmd = in->words[i];
+				cmdargs++;
+
+				if (++pipelen > MAX_PIPE_LEN) { /*check if pipe too long*/
+					fprintf(stderr, "Pipeline too deep.\n");
+					return 1;
+				}
+			}
+		} else { /*same command*/
+			switch (in->words[i][0]) {
+				case '|': 
+					if (outset == 2) { /*if out already set*/
+						fprintf(stderr, "%s: Ambiguous output.\n", curCmd);
+						return 1;
+					} else { /*reset cmdargs if new cmd coming*/
+						cmdargs = 0;
+						inset = 1;
+						outset = 0;
+					}
+					break;
+				case '<':
+					if (inset == 1) { /*if in already set by pipe*/
+						fprintf(stderr, "%s: Ambiguous input\n", curCmd);
+						exit(1);
+					} else if (inset == 2) { /*if in already set by redir*/
+						fprintf(stderr, "%s: Bad input redirection.\n", curCmd);
+						exit(1);
+					} else { /*if not already set*/
+						inset = 2;
+						if (in->words[i+1][0] == '|' || in->words[i+1][0] == '<' /* if name is missing*/
+							|| in->words[i+1][0] == '>' || in->words[i+1][0] == '\0') {
+							fprintf(stderr, "%s: Bad input redirection.\n", curCmd);
+							exit(1);
+						}
+					}
+					break;
+				case '>': 
+					if (outset == 2) { /*if out already set by redir*/
+						fprintf(stderr, "%s: Bad input redirection.\n", curCmd);
+						exit(1);
+					} else { /*if not already set*/
+						outset = 2;
+						if (in->words[i+1][0] == '|' || in->words[i+1][0] == '<' /* if name is missing*/
+							|| in->words[i+1][0] == '>' || in->words[i+1][0] == '\0') {
+							fprintf(stderr, "%s: Bad input redirection.\n", curCmd);
+							exit(1);
+						}
+					}
+					break;
+				default:
+					if (++cmdargs > MAX_CMND_NUM) { /*check if args too long*/
+						fprintf(stderr, "%s: Too many arguments.\n", curCmd);
+						exit(1);
+					}
+			}
+
 		}
-		if(str[i] == '|'){
-			flag = 0;
+	}
+	return 0;
+}
+
+void initInput(char *str, input *in) {
+	in->size = 0;
+	in->words[in->size++] = strtok(str, " \n");
+
+	while ((in->words[in->size++] = strtok(NULL, " \n"))) {
+		if (in->size == (10 * (10 + 3) + 1) 
+			&& in->words[in->size - 1] != NULL) {
+			break;
 		}
-		if(flag == 1){
-			printf("%s: Amibgious Input\n", tok);
-			exit(1);
-		}
-	}	
-	
+	}
+
+	in->size--; 
 }
--- parseline.h
+++ parseline.h
@@ -8,14 +8,28 @@
 #define MAX_CMND_ARGS 512
 #define MAX_PIPE_LEN 10
 #define MAX_CMND_NUM 10
+#define ARGC_MAX 10
+
+typedef struct stage{
+	int argc;
+	char argv[MAX_CMND_ARGS][ARGC_MAX];
+	char input[MAX_CMND_ARGS];
+	char output[MAX_CMND_ARGS];
+	int stageNum;
+} stage;
+
+typedef struct input {
+	char *words[MAX_CMND_ARGS]; /*3 extra args for name, <, >*/
+	int size;
+} input;
 
 void printLine(int, char *);
 void remove_newline_ch(char *);
-int checkDoubleAliOUT(char *str);
-int checkDoubleAliIN(char *str);
-int checkDoubleBars(char *str);
 char *getCommand(char *str);
-void checkAmibiousInput(char *str);
+int inputErrorCheck(input *in);
+void initInput(char *str, input *in);
+
+
 
 
 
